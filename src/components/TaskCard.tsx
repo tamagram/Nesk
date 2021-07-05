1d7fcd5a074be6958b419d20394d4c7fcb058e89
tamagram <tamagram64@gmail.com>
2021-07-05 19:42:35 +0900

    :sparkles: Add `TaskCard` component.

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import { Box, Button, Card, CardContent, Typography } from '@material-ui/core';
+import styled from 'styled-components';
 
-const TaskCard = () => {
+const TaskCard = styled(Card)`
+  margin: 10px;
+  padding: 10px;
+  display: Flex;
+  flex-direction: Column;
+  align-items: space-between;
+`;
+
+const Actions = styled.div`
+  align-self: flex;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const StyledTaskCard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <TaskCard>
+      <CardContent>
+        <Typography style={{ wordWrap: 'break-word' }} variant="h5" component="h2">
+          title
+        </Typography>
+        <Typography style={{ wordWrap: 'break-word' }} variant="body1">
+          texttextexttexttextexttexttextexttexttextextxttexttextexttexttextextxttexttextexttexttextextxttexttextexttexttextextxttexttextexttexttextextxttexttextexttexttextext
+          texttextexttexttextexttexttextexttexttextextttextextttextextttextextttextextttextextttextextttextextttextextttextextttextextttextext
+        </Typography>
+      </CardContent>
+      <Actions>
+        <Button variant="contained" color="secondary">skip</Button>
+        <Button variant="contained" color="primary">done</Button>
+      </Actions>
+    </TaskCard>
+  );
+};
 
-export default TaskCard
+export default StyledTaskCard;
