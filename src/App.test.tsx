d260f81652b8d233a007f80477e253db33ff2cb2
tamagram <tamagram64@gmail.com>
2021-07-07 08:23:45 +0900

    :white_check_mark: Run the snapshot test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/hello/i);
-  expect(linkElement).toBeInTheDocument();
+  const asFragment = render(<App />);
+  expect(asFragment).toMatchSnapshot();
 });
