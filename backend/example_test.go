d165e40e3fe272ae47c4387e9ed506ce3496723b
tamagram <tamagram64@gmail.com>
2021-07-10 06:28:52 +0900

    :white_check_mark: Add `Task` entity test.

--- backend/example_test.go
+++ backend/example_test.go
@@ -2,9 +2,11 @@ package task
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"entgo.io/ent/dialect"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/tamagram/nesk/backend/ent"
 )
 
@@ -12,7 +14,7 @@ func ExampleTask() {
 	// インメモリーのSQLiteデータベースを持つent.Clientを作成
 	client, err := ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
 	if err != nil {
-		log.Fatalf("failed to opening connection to sqlite: %v", err)
+		log.Fatalf("failed to opening connection to sqlite3: %v", err)
 	}
 	defer client.Close()
 	ctx := context.Background()
@@ -21,4 +23,11 @@ func ExampleTask() {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
 	// 出力
+	task1, err := client.Task.Create().Save(ctx)
+	if err != nil {
+		log.Fatalf("failed creating a Task: %v", err)
+	}
+	fmt.Println(task1)
+	// Output:
+	// Task(id=1, title=No title, details=No details)
 }
