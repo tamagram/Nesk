d0bfce0866bd7a2bd01d6d92c79745c7ce9e6c00
tamagram <tamagram64@gmail.com>
2021-07-21 00:14:01 +0900

    :white_check_mark: Add `GetTaskRequest` test for grpc service.

--- backend/pb_test.go
+++ backend/pb_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"testing"
 
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/tamagram/nesk/backend/ent/enttest"
 	"github.com/tamagram/nesk/backend/ent/proto/entpb"
 )
 
@@ -18,5 +21,36 @@ func TestTaskProto(t *testing.T) {
 	}
 	if got["Details"] != want["Details"] {
 		t.Fatalf("got %s want %s", got["Details"], want["Details"])
+	}
+}
+
+func TestGet(t *testing.T) {
+	// インメモリのsqliteインスタンスに接続されたentクライアントの初期化から始めます
+	ctx := context.Background()
+	client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	defer client.Close()
+
+	// 次に、Userサービスを初期化します。 ここでは、実際にポートを開いてgRPCサーバーを作成するのではなく
+	// ライブラリのコードを直接呼び出していることに注目してください。
+	svc := entpb.NewTaskService(client)
+
+	// 次に、ユーザーとカテゴリーを作成し、そのユーザーをカテゴリーの管理者に設定します。
+	task := client.Task.Create().
+		SetTitle("testTitle").
+		SetDetails("testDetails").
+		SaveX(ctx)
+
+	// 次に、エッジの情報なしでユーザーを取得します
+	got, err := svc.Get(ctx, &entpb.GetTaskRequest{
+		Id: int32(task.ID),
+	})
+	if err != nil {
+		t.Fatal("failed retrieving the created task", err)
+	}
+	if got.GetTitle() != "testTitle" {
+		t.Fatalf("got %s want %s", got.GetTitle(), "testTitle")
+	}
+	if got.GetDetails() != "testDetails" {
+		t.Fatalf("got %s want %s", got.GetDetails(), "testDetails")
 	}
 }
